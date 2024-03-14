a37e46094d219406a96bcd5a3be8a48c713459be
Your Name <you@example.com>
2024-03-14 23:13:37 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -73,6 +73,17 @@ arreglos en un tercer arreglo también ordenado de menor a mayor.
 
 void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
                        int result[]) {
+  int i, j, k = 0;
+  while (i < size1 && j < size2){
+    if (arr1[i] < arr2[j]){
+      result[k++] = arr1[i++];
+    }
+    else if (arr1[i] > arr2[j]){
+      result[k++] = arr2[j++];
+    }
+
+    
+  }
                        }
 
 /*
